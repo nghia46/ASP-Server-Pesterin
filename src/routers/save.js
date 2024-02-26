0d145420c22c9e628202aa28b9b6d34f0d652d24
TrinhHan1803 <trinhhan255@gmail.com>
2024-02-26 11:58:14 +0700

    editfuntion

--- src/routers/save.js
+++ src/routers/save.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import saveController from "../controllers/SaveController.js";
 
 router.post("/", saveController.saveArtToBookmark);
+router.delete("/",saveController.removeArtFromBookmark)
 
 export default router;
