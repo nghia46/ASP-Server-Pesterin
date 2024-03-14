133b22bc9b6307b5e1d11511107c84751464a3d8
Duy Lee <duylvd2002@gmail.com>
2024-03-14 20:56:03 +0700

    13/03/2024 Update Chat with image

--- src/services/CategoryServices.js
+++ src/services/CategoryServices.js
@@ -15,41 +15,6 @@ class CategoryService {
       throw error;
     }
   }
-
-  async searchCategoryByName(partialName) {
-    try {
-      const categories = await Category.find(
-        { name: { $regex: new RegExp(partialName, "i") } },
-        { name: 1, _id: 0 }
-      );
-      const response = categories.map((category) => category.name);
-
-      return response;
-    } catch (error) {
-      throw new Error(`Error searching category: ${error.message}`);
-    }
-  }
-
-  async getCategoryNames() {
-    try {
-      const categories = await Category.find({}, "name");
-      return categories.map((category) => ({
-        categoryName: category.name,
-        id: category._id,
-      }));
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async getCategoryById(categoryId) {
-    try {
-      const categories = await Category.find({ _id: categoryId });
-      return categories;
-    } catch (error) {
-      throw error;
-    }
-  }
 }
 
 export default new CategoryService();
