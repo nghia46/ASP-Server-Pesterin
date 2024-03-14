85386ffb863aed72098b8f48d2871136c015c2d3
nghia46 <nghialoe46@gmail.com>
2024-03-14 11:15:20 +0700

    fix hoài mà có dc đâu ...........

--- src/index.js
+++ src/index.js
@@ -24,7 +24,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 //CORS
-app.use(cors());
+app.use(cors({
+  origin : [
+    "https://asp-server-pesterin-production.up.railway.app"
+  ]
+}));
 
 //Connect to DB
 await db.connect();
